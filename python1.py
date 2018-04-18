d95a7eaa766a12797e89cb0d0cb60d6bb9d324b0
Angelika Poturalska <48386@student.ue.poznan.pl>
2018-04-18 08:54:06 +0200

    PoprawkaKolejna

---
+++ python1.py
@@ -0,0 +1,36 @@
+import string
+ile_dokumentow = 3
+dokumenty= []
+dokumenty.append("Your care set up, do not pluck my cARe down")
+dokumenty.append("My care, is loss of caRe with old care done.")
+dokumenty.append("Your CARE is gain of Care when new care iS won.")
+ile_zapytan = 2
+zapytania = []
+zapytania.append('care')
+zapytania.append('is')
+indeks_wyszukiwawczy = dict()
+
+
+nr_dokumentu = 0
+for i in dokumenty:
+    wyczyszczenie = i
+    for znak in string.punctuation:
+        wyczyszczenie = wyczyszczenie.replace(znak, ' ')
+
+    wyrazy = wyczyszczenie.lower().split()
+    for wyraz in wyrazy:
+        if wyraz not in indeks_wyszukiwawczy:
+            indeks_wyszukiwawczy[wyraz] = dict()
+        if nr_dokumentu not in indeks_wyszukiwawczy[wyraz]:
+            indeks_wyszukiwawczy[wyraz][nr_dokumentu] = 1
+        else:
+            indeks_wyszukiwawczy[wyraz][nr_dokumentu] += 1
+    nr_dokumentu += 1
+
+for zap in zapytania:
+    if zap in indeks_wyszukiwawczy:
+        print(indeks_wyszukiwawczy[zap])
+    else:
+        print([])
+        
+
